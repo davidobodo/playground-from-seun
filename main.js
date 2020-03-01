e7830a721a0c56e20dcb41b7a2272b3a4a8f1883
David Obodo <obododavid5@gmail.com>
2020-03-01 19:45:25 +0100

    get exact y position on page of dragged element

--- main.js
+++ main.js
@@ -1,28 +1,62 @@
 var checklist = document.querySelector(".checklist");
 var dragTarget;
 var dragBound;
-var dragBounds;
+var dragBounds = new Array();
 var getDragBounds = function () {
     var checklistItems = checklist.children;
     var itemLength = checklistItems.length;
     var previousItem;
     var currentItem;
     var lastItem;
+    // @TODO insert types/
     var previousItemBoundingRect;
     var currentItemBoundingRect;
     var lastItemBoundingRect;
     var topBoundary;
     var bottomBoundary;
-    var temporaryDragBounds;
+    var temporaryDragBounds = new Array();
     for (var i = 0; i <= itemLength; i++) {
         currentItem = i < itemLength ? checklistItems[i] : undefined;
         previousItem = i > 0 ? checklistItems[i - 1] : undefined;
         lastItem = checklistItems[itemLength - 1];
-        console.log({ currentItem: currentItem }, { previousItem: previousItem }, { lastItem: lastItem });
+        // console.log({ currentItem }, { previousItem }, { lastItem })
         currentItemBoundingRect = currentItem ? currentItem.getBoundingClientRect() : undefined;
         previousItemBoundingRect = previousItem ? previousItem.getBoundingClientRect() : undefined;
         lastItemBoundingRect = lastItem.getBoundingClientRect();
+        // console.log({ currentItemBoundingRect }, { previousItemBoundingRect }, { lastItemBoundingRect })
+        if (i == 0) {
+            topBoundary = currentItemBoundingRect.top;
+            bottomBoundary = currentItemBoundingRect.top + currentItemBoundingRect.height * 0.5;
+            // let bottomBoundary1 = currentItemBoundingRect.bottom; //why not this?
+            // console.log({ topBoundary }, { bottomBoundary })
+        }
+        else if (i === itemLength) {
+            topBoundary = lastItemBoundingRect.top + lastItemBoundingRect.height * 0.5;
+            bottomBoundary = lastItemBoundingRect.top + lastItemBoundingRect.height;
+            // console.log({ topBoundary }, { bottomBoundary })
+        }
+        else {
+            topBoundary = previousItemBoundingRect.top + previousItemBoundingRect.height * 0.5;
+            bottomBoundary = currentItemBoundingRect.top + currentItemBoundingRect.height * 0.5;
+            // console.log({ topBoundary }, { bottomBoundary })
+        }
+        temporaryDragBounds.push({
+            index: i,
+            topBoundary: topBoundary,
+            bottomBoundary: bottomBoundary,
+            operation: i < itemLength ? "insert" : "append"
+        });
+        // console.log({ temporaryDragBounds })
     }
+    return temporaryDragBounds;
+};
+var getDragBoundForCurrentPosition = function (e) {
+    // get exact position of dragTarget on page
+    var positionY = e.pageY;
+    // console.log({ dragBounds })
+    // console.log({ positionY })
+    // console.log(dragBounds.find(o => positionY >= o.topBoundary && positionY <= o.bottomBoundary))
+    return dragBounds.find(function (o) { return positionY >= o.topBoundary && positionY <= o.bottomBoundary; });
 };
 var handleMousedown = function (e) {
     dragTarget = e.target;
@@ -34,7 +68,9 @@ var handleDragStart = function (e) {
     //responsible for making drag capability only enabled on blue circle
     if (dragHandle.contains(dragTarget)) {
         var scopedDragBounds = getDragBounds();
-        // dragBounds = scopedDragBounds;
+        // console.log({ scopedDragBounds })
+        dragBounds = scopedDragBounds;
+        // console.log({ dragBounds })
         dragBound = undefined;
         dragTarget = undefined;
     }
@@ -43,6 +79,9 @@ var handleDragStart = function (e) {
     }
 };
 var handleDrag = function (e) {
+    e.stopPropagation();
+    var scopedDragBounds = getDragBoundForCurrentPosition(e);
+    console.log({ scopedDragBounds: scopedDragBounds });
 };
 var handleDragEnd = function (e) {
 };
--- main.ts
+++ main.ts
@@ -1,16 +1,24 @@
+interface DragBounds {
+    index: number,
+    topBoundary: number,
+    bottomBoundary: number,
+    operation: "insert" | "append"
+}
+
 let checklist = document.querySelector(".checklist");
 let dragTarget: EventTarget;
-let dragBound;
-let dragBounds;
+let dragBound: DragBounds;
+let dragBounds = new Array<DragBounds>();
 
 const getDragBounds = () => {
     const checklistItems = checklist.children;
     const itemLength = checklistItems.length;
 
-    let previousItem;
-    let currentItem;
-    let lastItem;
+    let previousItem: Element;
+    let currentItem: Element;
+    let lastItem: Element;
 
+    // @TODO insert types/
     let previousItemBoundingRect;
     let currentItemBoundingRect;
     let lastItemBoundingRect;
@@ -18,18 +26,56 @@ const getDragBounds = () => {
     let topBoundary;
     let bottomBoundary;
 
-    let temporaryDragBounds;
+    let temporaryDragBounds = new Array<DragBounds>();
 
     for (let i = 0; i <= itemLength; i++) {
         currentItem = i < itemLength ? checklistItems[i] : undefined;
         previousItem = i > 0 ? checklistItems[i - 1] : undefined;
         lastItem = checklistItems[itemLength - 1];
-        console.log({ currentItem }, { previousItem }, { lastItem })
+        // console.log({ currentItem }, { previousItem }, { lastItem })
 
         currentItemBoundingRect = currentItem ? currentItem.getBoundingClientRect() : undefined;
         previousItemBoundingRect = previousItem ? previousItem.getBoundingClientRect() : undefined;
         lastItemBoundingRect = lastItem.getBoundingClientRect();
+        // console.log({ currentItemBoundingRect }, { previousItemBoundingRect }, { lastItemBoundingRect })
+
+        if (i == 0) {
+            topBoundary = currentItemBoundingRect.top;
+            bottomBoundary = currentItemBoundingRect.top + currentItemBoundingRect.height * 0.5;
+            // let bottomBoundary1 = currentItemBoundingRect.bottom; //why not this?
+            // console.log({ topBoundary }, { bottomBoundary })
+
+        } else if (i === itemLength) {
+            topBoundary = lastItemBoundingRect.top + lastItemBoundingRect.height * 0.5;
+            bottomBoundary = lastItemBoundingRect.top + lastItemBoundingRect.height;
+            // console.log({ topBoundary }, { bottomBoundary })
+
+        } else {
+            topBoundary = previousItemBoundingRect.top + previousItemBoundingRect.height * 0.5;
+            bottomBoundary = currentItemBoundingRect.top + currentItemBoundingRect.height * 0.5;
+            // console.log({ topBoundary }, { bottomBoundary })
+        }
+
+        temporaryDragBounds.push({
+            index: i,
+            topBoundary,
+            bottomBoundary,
+            operation: i < itemLength ? "insert" : "append"
+        })
+
+        // console.log({ temporaryDragBounds })
     }
+
+    return temporaryDragBounds;
+}
+
+const getDragBoundForCurrentPosition = (e: DragEvent) => {
+    // get exact position of dragTarget on page
+    const positionY = e.pageY;
+    // console.log({ dragBounds })
+    // console.log({ positionY })
+    // console.log(dragBounds.find(o => positionY >= o.topBoundary && positionY <= o.bottomBoundary))
+    return dragBounds.find(o => positionY >= o.topBoundary && positionY <= o.bottomBoundary)
 }
 
 const handleMousedown = (e: DragEvent) => {
@@ -45,7 +91,11 @@ const handleDragStart = (e: DragEvent) => {
     //responsible for making drag capability only enabled on blue circle
     if (dragHandle.contains(dragTarget as any)) {
         const scopedDragBounds = getDragBounds();
-        // dragBounds = scopedDragBounds;
+        // console.log({ scopedDragBounds })
+
+        dragBounds = scopedDragBounds;
+        // console.log({ dragBounds })
+
         dragBound = undefined;
         dragTarget = undefined;
     } else {
@@ -54,6 +104,9 @@ const handleDragStart = (e: DragEvent) => {
 };
 
 const handleDrag = (e: DragEvent) => {
+    e.stopPropagation();
+    const scopedDragBounds = getDragBoundForCurrentPosition(e);
+    console.log({ scopedDragBounds })
 
 };
 
